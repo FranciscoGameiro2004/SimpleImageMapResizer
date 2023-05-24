24d5c3e48a74c2c73dceb3b2da61d82911ce12f2
Francisco Gameiro <gameirofrancisco6@gmail.com>
2023-05-24 20:39:45 +0100

    Ajustamentos

--- imageMapResizer.js
+++ imageMapResizer.js
@@ -1,6 +1,10 @@
-let previousSize = 0
+let previousSize = []
+
 addEventListener('DOMContentLoaded',()=>{
-    previousSize = document.querySelector('.img').naturalHeight
+    previousSize = []
+    document.querySelectorAll('.img-map').forEach(image => {
+        previousSize.push(image.naturalHeight)
+    });
 
     resizeMap()
 
@@ -10,22 +14,35 @@ addEventListener('DOMContentLoaded',()=>{
 })
 
 function resizeMap(){
-    let naturalSize = previousSize
-    let size = document.querySelector('.img').height
-    const multiplier = size/naturalSize
-    console.log(multiplier);
+    let multiplier = []
+    let naturalSize = []
+    let size = []
 
-    const map = document.querySelectorAll('.map > area')
-
-    map.forEach(area => {
-        let coords = area.getAttribute('coords').split(',')
-        coords.forEach((number, index) => {
-            coords[index] = (number*multiplier)
-        });
-        area.setAttribute('coords', coords.join(','))
-        console.log(area)
+    previousSize.forEach((prev, index) => {
+        naturalSize.push(prev)
+        size.push(document.querySelectorAll('.img-map')[index].height)
     });
 
-    console.log('lol');
-    previousSize = size
+    previousSize.forEach((element, index) => {
+        multiplier.push(size[index]/naturalSize[index])
+    })
+
+    const map = document.querySelectorAll('.map')
+    console.log(map.length)
+    map.forEach((nodes, mapIndex) => {
+        nodes.childNodes.forEach(node => {
+            if (node.tagName === 'AREA') {
+                let coords = node.getAttribute('coords').split(',')
+                coords.forEach((number, coordsIndex) => {
+                    coords[coordsIndex] = (number*multiplier[mapIndex])
+                });
+                node.setAttribute('coords', coords.join(','))
+            }
+            
+        });
+    });
+
+    for (i=0;i<=previousSize.length - 1; i++)[
+        previousSize[i] = size[i]
+    ]
 }
